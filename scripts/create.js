9f017b3f7c4189804f233a32e421f0b365f61022
Paula Rodrigues <paulinhaabro@gmail.com>
2022-06-29 16:12:07 -0300

    edit for better programming practices

--- scripts/create.js
+++ scripts/create.js
@@ -1,18 +1,19 @@
 function renderCreateQuiz() {
-  document.querySelector("main").innerHTML = ` <div class="quizz-start">
-  <h2>Comece pelo começo</h2>
-  <div class="basic-informations">
-    <input class="quizz-title" type="text" placeholder="Título do seu quizz">
-    <p class="alert quizz-title hide">Seu título deve ter entre 20 a 65 caracteres</p>
-    <input class="quizz-image" type="text" placeholder="URL da imagem do seu quizz">
-    <p class="alert quizz-image hide">Deve ser uma URL válida</p>
-    <input class="quizz-question-number" type="text" placeholder="Quantidade de perguntas do quizz">
-    <p class="alert quizz-question-number hide">Deve haver no mínimo 3 perguntas</p>
-    <input class="quizz-levels-number" type="text" placeholder="Quantidades de níveis do quizz">
-    <p class="alert quizz-levels-number hide">Deve haver no mínimo 2 níveis</p>
-  </div>
-  <button onclick="getValues()">Prosseguir para criar perguntas</button>
-</div>
+  document.querySelector("main").innerHTML = ` 
+    <div class="quizz-start">
+      <h2>Comece pelo começo</h2>
+      <div class="basic-informations">
+        <input class="quizz-title" type="text" placeholder="Título do seu quizz">
+        <p class="alert quizz-title hide">Seu título deve ter entre 20 a 65 caracteres</p>
+        <input class="quizz-image" type="text" placeholder="URL da imagem do seu quizz">
+        <p class="alert quizz-image hide">Deve ser um URL válido</p>
+        <input class="quizz-question-number" type="text" placeholder="Quantidade de perguntas do quizz">
+        <p class="alert quizz-question-number hide">Deve haver no mínimo 3 perguntas</p>
+        <input class="quizz-levels-number" type="text" placeholder="Quantidades de níveis do quizz">
+        <p class="alert quizz-levels-number hide">Deve haver no mínimo 2 níveis</p>
+      </div>
+      <button onclick="getValues()">Prosseguir para criar perguntas</button>
+    </div>
   `;
 }
 
@@ -32,7 +33,7 @@ function getValues() {
   let questionNumberOk = verifyQuestionNumber();
   let LevelNumberOk = verifyLevelNumber();
   if (titleOk && questionNumberOk && LevelNumberOk) {
-    alert("ttudo certo, vamos lá");
+    alert("tudo certo, vamos lá");
   } else {
     alert("rever dados");
   }
@@ -43,27 +44,24 @@ function verifyQuizzTitle() {
   if (titleLength < 20 || titleLength > 65) {
     alert("titulo errado");
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
 function verifyQuestionNumber() {
   if (quizzQuestionNumber < 3) {
     alert("Questões menor que 3");
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
 function verifyLevelNumber() {
   if (quizzLevelNumber < 2) {
     alert("Menos que 2 níveis");
     return false;
-  } else {
-    return true;
   }
+  return true;
 }
 
 let quizzTitle = null;
